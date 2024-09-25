657bf3518cf1eca0b2f87aa3d0736568bf33fda0
HalfMoon1008 <yunsh1008@naver.com>
2024-09-25 18:50:16 +0900

    update comment

--- clip/clip.py
+++ clip/clip.py
@@ -1,3 +1,9 @@
+# 이 파일은 서비스 배포에 치중된 파일...!
+# 당장의 연구에서 이 정도까지 할 필요는 없을 듯 하다..!
+# 아키텍쳐 구현을 좀 더 봐야할 듯 함...!
+
+## Q) = 좀 더 알아보기
+
 from .model import build_model # 모델을 생성하는 함수
 from .simple_tokenizer import SimpleTokenizer as _Tokenizer
 
@@ -7,7 +13,7 @@ import os
 import urllib  # URL에서 파일을 다운로드할 때 사용하는 라이브러리
 import warnings  # 경고 메시지를 표시하기 위한 라이브러리 (특정 조건에서 경고를 줄 때 사용됨)
 from packaging import version  # 버전 비교를 위한 라이브러리 (소프트웨어의 버전을 비교할 때 사용됨)
-from typing import Union, List  # 타입 힌트(코드 가독성을 높이고 오류를 줄여줌)를 위한 라이브러리
+from typing import Union, List  # 타입 힌트(코드 가독성을 높이고 오류를 줄여줌)를 위한 라이브러리, __all__ Q) 형변환 자세하게 알아보자
 
 import torch  
 from PIL import Image  # PIL은 이미지를 처리하고 조작하기 위한 라이브러리임
@@ -16,6 +22,7 @@ from tqdm import tqdm
 
 # 이미지 보간(interpolation) 방법을 설정함. 보간은 이미지 크기를 조정할 때 픽셀 값을 계산하는 방법
 # 만약 최신 버전의 'InterpolationMode'가 있으면 그걸 사용하고, 없으면 기존의 'BICUBIC'을 사용
+# interpolationmode : 기존의 픽셀값을 기준으로 새로운 픽셀을 추정
 try:
     from torchvision.transforms import InterpolationMode
     BICUBIC = InterpolationMode.BICUBIC  # 이미지 품질이 좋은 BICUBIC 방식을 사용
@@ -37,6 +44,10 @@ __all__ = ["available_models", "load", "tokenize"]
 # tokenize: 문자열(텍스트)을 토큰화하여 모델이 처리할 수 있는 형태로 변환하는 역할
 # 텍스트 데이터를 입력으로 사용할 때 필수적인 전처리 과정이기 때문
 
+# ex) def available_models() -> List[str]:
+# from typing import Union, List을 사용해서 위와 같이 함수를 정의 할 수 있다.
+# 형 변환이 편해짐
+# Q)
 
 # 텍스트를 토큰화하는 데 사용할 토크나이저 객체를 생성
 _tokenizer = _Tokenizer()
@@ -59,7 +70,7 @@ def _download(url: str, root: str):
     os.makedirs(root, exist_ok=True)  # 파일이 저장될 디렉토리가 없으면 새로 만듦
     filename = os.path.basename(url)  # URL에서 파일 이름만 추출
 
-    expected_sha256 = url.split("/")[-2]  # URL에서 예상되는 해시 값을 추출
+    expected_sha256 = url.split("/")[-2]  # URL에서 예상되는 해시 값을 추출, sha256 : URL(문자열)을 가져올 수 있는 해시 알고리즘 Q)
     download_target = os.path.join(root, filename)  # 파일이 저장될 경로를 설정
 
     # 파일이 이미 존재하지만 정상적인 파일이 아니면 오류를 발생
@@ -76,9 +87,9 @@ def _download(url: str, root: str):
     # URL에서 파일을 다운로드
     with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
         # 파일을 다운로드하는 동안 진행률을 표시
-        with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True, unit_divisor=1024) as loop:
+        with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True, unit_divisor=1024) as loop: # iB : 이진바이트
             while True:
-                buffer = source.read(8192)  # 8192 byte씩 데이터를 읽음 _ why? 8192?
+                buffer = source.read(8192)  # 8192 byte씩 데이터를 읽음 _ A) 8192는 파일을 다운로드하거나 복사할 때 효율적인 데이터 전송을 위해 자주 사용되는 버퍼 크기 (성능 최적화, 표준 값)
                 if not buffer:
                     break  # 더 이상 읽을 데이터가 없으면 종료함
 
@@ -112,6 +123,7 @@ def available_models() -> List[str]:
 
 # CLIP 모델을 불러오는 함수
 def load(name: str, device: Union[str, torch.device] = "cuda" if torch.cuda.is_available() else "cpu", jit: bool = False, download_root: str = None):
+    # JIT : Just-In-Time, 사전에 기계어로 모두 번역하는 컴파일이 아닌 line by line으로 필요한 부분만 컴파일
     """Load a CLIP model
 
     Parameters
@@ -157,37 +169,58 @@ def load(name: str, device: Union[str, torch.device] = "cuda" if torch.cuda.is_a
             state_dict = torch.load(opened_file, map_location="cpu")
 
     # JIT가 꺼져 있으면 모델을 빌드하고 장치에 올림
+    # Q) model.state_dict()에 대해 좀 더 알아보기
     if not jit:
         model = build_model(state_dict or model.state_dict()).to(device)
         if str(device) == "cpu":  # CPU에 모델을 올렸으면 float 형식으로 변환
-            model.float()
-        return model, _transform(model.visual.input_resolution)
+            model.float() # model.state_dict()로 하면 K와 V의 dict형태로 저장되는데, 이때 V가 tensor값이기 때문에 float로 변환 됨
+        return model, _transform(model.visual.input_resolution) # 전처리 파이프라인에 해당 Q)
 
     # 장치 이름을 패치
-    device_holder = torch.jit.trace(lambda: torch.ones([]).to(torch.device(device)), example_inputs=[])
+    device_holder = torch.jit.trace(lambda: torch.ones([]).to(torch.device(device)), example_inputs=[]) # 내 코드가 돌아가는 환경이 CPU인지, GPU인지 추적하는 코드
+        # torch.jit.trace : 주어진 함수의 실행을 추적하고, 그 함수가 실행되는 과정을 기록하여 그래프 형태로 저장
+        # torch.one([]) : 크키가 없는 텐서를 만듬. 이 텐서는 device 정보를 설정하는데 사용
+        # .to(torch.device(device))를 통해 텐서를 지정된 장치로 이동시킴, device = 'cuda'면 텐서가 GPU로, device = 'cpu'면 텐서가 CPU로 이동함
     device_node = [n for n in device_holder.graph.findAllNodes("prim::Constant") if "Device" in repr(n)][-1]
+        # prim::Constant : JIT 컴파일 시 장치 정보와 관련된 상수를 나타냄
+        # [-1]로 마지막 장치 정보에 대한 정보를 tensor에 저장
+        # 즉, JIT 모델 내 장치 설정을 반영해서 다른 사람이 이 코드를 사용하더라도 의도된 장비(GPU or CPU etc..)에서 코드가 돌아가도록 확인시키는 코드
+
+        # JIT는 코드를 작성할 때 부터 들어가있는 코드 작성자의 배려로 느껴짐
+        # "이 코드를 clone해가서 쓸 당신들의 환경에서 편의를 좀 더 제공하고자 내가 설정을 미리 해줄게"
+    
 
     def _node_get(node: torch._C.Node, key: str):
         """Gets attributes of a node which is polymorphic over return type.
         
         From https://github.com/pytorch/pytorch/pull/82628
         """
-        sel = node.kindOf(key)
-        return getattr(node, sel)(key)
+        sel = node.kindOf(key) # kindOf : (key)의 타입을 반환. 정수, 문자열, 장치 정보 등등
+        return getattr(node, sel)(key) # getattr : 동적으로 객체의 속성을 가져오는 함수
+    
+        # 모델이 GPU에서 실행되도록 설정되어 있는지 확인하고 싶을 때, "Device"라는 키를 사용하여 해당 속성을 가져옴
+        # 속성의 반환값은 'cuda' or 'cpu' 등이 됨
 
     def patch_device(module):
         try:
-            graphs = [module.graph] if hasattr(module, "graph") else []
+            graphs = [module.graph] if hasattr(module, "graph") else [] 
+            # module.graph: module 객체가 JIT 컴파일된 모델일 경우, 그 모델은 그래프 형태로 컴파일
+            # 그래프는 모델의 연산을 나타내며, 모든 연산과 관련된 노드들을 포함
         except RuntimeError:
             graphs = []
 
         if hasattr(module, "forward1"):
             graphs.append(module.forward1.graph)
+            # 일부 모듈은 'forward1' 이라는 또 다른 그래프를 가질 수 있고, 그래프에 추가함
 
         for graph in graphs:
             for node in graph.findAllNodes("prim::Constant"):
                 if "value" in node.attributeNames() and str(_node_get(node, "value")).startswith("cuda"):
                     node.copyAttributes(device_node)
+                    # 어떤 deive든! node의 형태로 model.dict()에 있는 value들을 가져오겠다.
+
+        # JIT 컴파일된 모델이 실행되는 동안 장치 정보(CPU 또는 GPU)가 잘못 설정되거나, 시스템 환경에 맞지 않게 설정될 경우 모델이 제대로 작동하지 않을 수 있음
+        # 이를 방지하기 위해 장치 정보를 추적하고, 필요시 수정(forward1을 사용해서)하여 모델이 의도한 대로 실행되도록 보장하는 역할
 
     model.apply(patch_device)
     patch_device(model.encode_image)
